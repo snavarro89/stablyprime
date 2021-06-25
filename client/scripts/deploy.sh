39405783a84d5df9b34aab0143c4213c3368525e
Samuel I. Navarro Riojas <snavarroriojas@gmail.com>
2021-06-25 12:23:49 -0500

    Fix Typo on client deploy script

--- client/scripts/deploy.sh
+++ client/scripts/deploy.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 #upload files
-docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd)/dist/fuse:/aws amazon/aws-cli --profile aws-fluxy-s3 s3 cp . s3://fluxy-production --recursive --include "*" --acl public-read --cache-control public,max-age=31536000,no-transform
-docker run --rm -it -v ~/.aws:/root/.aws amazon/aws-cli --profile aws-fluxy-cloudfront cloudfront create-invalidation --distribution-id E378K7IAQVS246 --paths "/index.html"
+docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd)/dist/stablyprime:/aws amazon/aws-cli --profile aws-stably-s3 s3 cp . s3://stableprime-production --recursive --include "*" --acl public-read --cache-control public,max-age=31536000,no-transform 
+docker run --rm -it -v ~/.aws:/root/.aws amazon/aws-cli --profile aws-stably-cloudfront cloudfront create-invalidation --distribution-id E2I68KBA8VDEF2 --paths "/index.html"
