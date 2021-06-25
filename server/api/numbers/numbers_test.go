ffc4cede53fa3edc73314d50b2b1c2725df56ae5
Samuel I. Navarro Riojas <snavarroriojas@gmail.com>
2021-06-25 12:32:09 -0500

    When input was either 0, 1 or 2 we were returning the same number as the largest prime, but we need to send the largest prime from n-1. Since there are no prime numbers before 2 those 3 values should return That there are no prime numbers.

--- server/api/numbers/numbers_test.go
+++ server/api/numbers/numbers_test.go
@@ -1,6 +1,7 @@
 package numbers
 
 import (
+	"fmt"
 	"testing"
 
 	A "github.com/snavarro89/stablyprime/app"
@@ -26,9 +27,10 @@ func TestOne(t *testing.T) {
 		},
 	}
 	prime, err := app.Data.GetPrime(1)
-	expect := 1
-	if err != nil || prime != 1 {
-		t.Fatalf(`GetPrime(1) = %b, %v, want match for %b, nil`, prime, err, expect)
+	expect := 0
+	expectError := fmt.Sprintf("There are no prime numbers before %d", 1)
+	if err == nil || prime != 0 {
+		t.Fatalf(`GetPrime(1) = %b, %v, want match for %b, %v`, prime, err, expect, expectError)
 	}
 }
 
